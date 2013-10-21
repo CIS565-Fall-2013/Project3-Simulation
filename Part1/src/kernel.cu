c25a4168cd7f8795f20c7c2daa192c5d664d4e89
nmarshak1337 <nmarshak@seas.upenn.edu>
2013-10-21 16:37:21 -0400

    Collision detection is now back in!

--- Part1/src/kernel.cu
+++ Part1/src/kernel.cu
@@ -293,20 +293,19 @@ void updateF(int N, float dt, glm::vec4 * pos, glm::vec3 * vel, glm::vec3 * acc)
     glm::vec4 my_pos;
 	glm::vec3 my_vel;
     glm::vec3 accel;
-	//glm::vec3 newVel;
+	glm::vec3 newVel;
 
     if(index < N){
 		my_pos = pos[index];
-		//my_vel = vel[index];
+		my_vel = vel[index];
 	}
 
     accel = ACC(N, my_pos, pos);
-	//newVel = resolveCollisions(N, my_pos, pos, my_vel);
+	newVel = resolveCollisions(N, my_pos, pos, my_vel);
 
     if(index < N){
-		//acc[index] = accel + newVel;
 		acc[index] = accel;
-		//vel[index] = newVel;
+		vel[index] = newVel;
 	}
 }
 
