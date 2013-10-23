28f76dadde627cf16a5e475a24578cee0cb42c69
nmarshak1337 <nmarshak@seas.upenn.edu>
2013-10-22 23:54:37 -0400

    We've proved that we change rotation dynamically. Now it's time to actually apply rotations and torques.

--- Part2/src/kernel.cu
+++ Part2/src/kernel.cu
@@ -429,7 +429,8 @@ void sendToVBO(int N, glm::vec4 * pos, float * vbo, int width, int height, float
         vbo[4*index+0] = pos[index].x*c_scale_w;
         vbo[4*index+1] = pos[index].y*c_scale_h;
         vbo[4*index+2] = 0;
-        vbo[4*index+3] = ((float)index)/N * TWO_PI;
+        //vbo[4*index+3] = ((float)index)/N * TWO_PI;
+		vbo[4*index+3] = (pos[index].x)/N * TWO_PI;
     }
 }
 
